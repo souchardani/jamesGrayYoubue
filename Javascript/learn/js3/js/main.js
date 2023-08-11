45ea7394fd5c061730182529de4fc13daf438f0e
souchardani <danielsou9782@gmail.com>
2023-08-11 11:37:52 +0200

    seccion 4

---
+++ Javascript/learn/js3/js/main.js
@@ -0,0 +1,5 @@
+//Strings
+const myVariable = "Mathematics";
+
+//The lenght Property
+console.log("dani,diego,iraida".split(","));
---
+++ Javascript/learn/js4/js/main.js
@@ -0,0 +1,10 @@
+const myNumber = 42;
+
+const myFloat = 42.01;
+
+const myString = "42.263sdfsdbv";
+
+//numbers Methods
+//the number.isInteger() method determines wether the passed value is an integer
+
+console.log(Number("Dani"));
