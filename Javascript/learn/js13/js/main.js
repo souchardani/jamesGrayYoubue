39aec3d593dabf512bc8ef0daa41101175657ea3
souchardani <danielsou9782@gmail.com>
2023-09-14 16:18:12 +0200

    seccion 13 finalizada

--- Javascript/learn/js13/js/main.js
+++ Javascript/learn/js13/js/main.js
@@ -1,4 +1,25 @@
-console.log("Hello World!");
-console.log(typeof "Dave");
-console.log(typeof 42);
-console.log(typeof true);
+//Functions
+
+//Methods = Built in functions!
+const toProperCase = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
+
+console.log(toProperCase("dANIeL"));
+
+//Functions provide reusable code
+
+// function sum(num1, num2) {
+//   if (num2 === undefined) {
+//     return num1 + num1;
+//   }
+//   return num1 + num2;
+// }
+
+// console.log(sum(5, 10));
+
+// const getUserNameFromEmail = (email) => {
+//     return email.slice(0, email.indexOf("@"));
+//   };
+
+//   console.log(getUserNameFromEmail("daniel@github.com"));
